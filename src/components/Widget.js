faa0f1e1a44b1d8218d09f8fc8869dc96ed0e18b
zero <moneymavie@gmail.com>
2024-03-06 12:06:49 -0500

    changes to array to display widget

--- src/components/Widget.js
+++ src/components/Widget.js
@@ -1,7 +1,21 @@
 import react from 'react';
 
-const Widget = ()=>{
-return <section className="widget">Widget</section>
+const Widget = ({color})=>{
+
+    //array destructuring
+    // const array1 = [1, 2, 3];
+    // const [one, two] = array1; (only 1 and 2 will be assigned to one and two respectively)
+
+
+    // const object = { orange: 'orange', mango:'mango', apple:'apple'};
+    // const {orange, , apple};
+    //leave blank space between commas to skip one key from object to be destructured
+    
+
+    const widgetStyles = {
+        background: color
+    }
+return <section className="widget" style={widgetStyles}>Widget</section>
 }
 
 export default Widget;
--- src/components/WidgetWrapper.js
+++ src/components/WidgetWrapper.js
@@ -1,9 +1,16 @@
-import react from 'react';
-import Widget from './Widget';
+import react from "react";
+import Widget from "./Widget";
 
-const WidgetWrapper = () =>{
-    return <nav className='widgetWrapper widgetGridContainer'><Widget/><Widget/><Widget/><Widget/></nav>
-}
+const WidgetWrapper = () => {
+  let getWidget = () => {
+    let newArr = [];
+    for (let i = 0; i < 4; i++) {
+      newArr.push(<Widget />);
+    }
+    return newArr;
+  };
+  return <nav className="widgetWrapper widgetGridContainer">{getWidget()}<Widget color="green"/><Widget color="#00f" /><Widget color="rgb(200, 100, 100)" /><Widget color="yellow"/></nav>;
 
+};
 
 export default WidgetWrapper;
